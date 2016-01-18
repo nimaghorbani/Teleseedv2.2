1c13c23946d7760d0f5a1c64571d0c02bf7c46a3
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-18 00:53:41 -0800

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'teleseed' then -- Put everything you like :)
+  if matches[1]:lower() == 'thor' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /thor ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "teleseed" then -- Put everything you like :)
+    if matches[2] == "thor" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -141,8 +141,13 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (thor)",-- Put everything you like :)
+    "^[!/]([Tt]hor)"-- Put everything you like :)
+    "^([S|s]tats)$",
+    "^([S|s]tatslist)$",
+    "^([S|s]tats) (group) (%d+)",
+    "^([S|s]tats) (thor)",-- Put everything you like :)
+    "^([T|t]hor)"-- Put everything you like :)
     }, 
   run = run
 }
