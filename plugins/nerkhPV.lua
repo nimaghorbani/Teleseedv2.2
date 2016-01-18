20f90102a08e05bcb1c1e7802b75ebbed6414ab8
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-18 01:59:53 -0800

    Create nerkhPV.lua

---
+++ plugins/nerkhPV.lua
@@ -0,0 +1,12 @@
+do
+    function run(msg, matches)
+	return 'Nerkh Sakht GP 1 Mahe \n 2000 Toman Kart Sharj \n Nerkh Sakht GP Daemi \n 5000 Toman Kart Sharj \n Creator: @Hoo3instar'
+end
+return {
+  patterns = {
+      "^[/!%&$]([N|n]erkh)$",
+      "^([N|n]erkh)$"
+      }, 
+  run = run 
+}
+end
