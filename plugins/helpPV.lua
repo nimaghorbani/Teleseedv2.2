387100dfe60faac458cbcac3490b12feded7f455
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-18 01:55:53 -0800

    Create helpPV.lua

---
+++ plugins/helpPV.lua
@@ -0,0 +1,12 @@
+do
+    function run(msg, matches)
+	return 'For Help & new News \n From Thor-Bot \n join With The under Channel \n  https://telegram.me/ThorBotCh /n Creator & Programer : @MrGhor & @@Hoo3instar'
+end
+return {
+  patterns = {
+      "^[/!%&$]([H|h]elp)$",
+      "^([H|h]elp)$"
+      }, 
+  run = run 
+}
+end
