90fad176000ba8a36fac93792f383a9552d0d848
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-18 01:59:21 -0800

    Create creategpPV.lua

---
+++ plugins/creategpPV.lua
@@ -0,0 +1,12 @@
+do
+    function run(msg, matches)
+	return 'For CreateGroup By Thor-Bot \n Please Send PM To \n @MrGhor Or @Hoo3instar \n Baraye Dastresi B Hazine GP \n Ersal /nerkh b PV bot . '
+end
+return {
+  patterns = {
+      "^[/!%&$]([C|c]reategp)$",
+      "^([C|c]rategp)$"
+      }, 
+  run = run 
+}
+end
